1ab38011c3a990d03d75019941977a13b2bdd6e9
Jodelfi <jodelfimarimba56@gmail.com>
2024-07-21 12:09:36 +0100

    style: adicionei a página de newsletter

--- newsletter.php
+++ newsletter.php
@@ -1,3 +1,5 @@
+<?php require_once 'controllers/conection.php'; ?>
+
 <!DOCTYPE html>
 
 <html lang="pt">
@@ -7,23 +9,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content=" ">
 
-    <title> +Kwanza | Newsletter | Título da notícia</title>
+    <title> +Kwanza | sobre nós</title>
 
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet">
-    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     <!-- Icons -->
-
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    
     <!-- Style -->
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/objects.css">
-    <link rel="stylesheet" href="../assets/css/root.css">
-    <link rel="stylesheet" href="../assets/css/midiaQ.css">
-    <link rel="stylesheet" href="../assets/css/animations.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/objects.css">
+    <link rel="stylesheet" href="./assets/css/root.css">
+    <link rel="stylesheet" href="./assets/css/midiaQ.css">
+    <link rel="stylesheet" href="./assets/css/animations.css">
 </head>
 
 <body>
@@ -31,17 +32,19 @@
     <!-- Header -->
     <header class="flex a-center j-between normal-font small-sz">
         <section class="logo-container">
-            <img src="../assets/img/logo.svg" alt="Logo do +Kwanza" class="logo">
+            <a href="">
+                <img src="./assets/img/logo.svg" alt="Logo do +Kwanza" class="logo">
+            </a>
         </section>
 
         <!-- Destop Navgation -->
-        <nav class="menu-desktop hide-mobile flex row j-around a-center">
+        <nav class="navbar menu-desktop hide-mobile flex row j-around a-center">
 
             <ul class="flex row">
-                <li><a href="" class="nav-link">Home</a></li>
-                <li><a href="" class="nav-link">Podcast</a></li>
-                <li><a href="" class="nav-link">Sobre nós</a></li>
-                <li><a href="" class="nav-link">Contactos</a></li>
+                <li><a href="./index.php#home" class="page-scroll nav-link">Home</a></li>
+                <li><a href="./index.php#podcast" class="page-scroll nav-link">Podcast</a></li>
+                <li><a href="./sobrenos.php#about" class="page-scroll nav-link">Sobre nós</a></li>
+                <li><a href="./index.php#contact" class="page-scroll nav-link">Contactos</a></li>
             </ul>
 
         </nav>
@@ -51,13 +54,13 @@
             <nav class="navbar menu-mobile">
                 <li>
                     <a href="#" class="flex row" id="menu-icon">
-                        <img class="icon-menu" src="../assets/img/icon/menu.webp" alt="menu">
+                        <img class="icon-menu" src="./assets/img/icon/menu.webp" alt="menu">
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a href="" class="nav-link">Home</a></li>
-                        <li><a href="" class="nav-link">Podcast</a></li>
-                        <li><a href="" class="nav-link">Sobre nós</a></li>
-                        <li><a href="" class="nav-link">Contactos</a></li>
+                        <li><a href="./index.php#home" class="page-scroll nav-link">Home</a></li>
+                        <li><a href="./index.php#podcast" class="page-scroll nav-link">Podcast</a></li>
+                        <li><a href="./sobrenos.php#about" class="page-scroll nav-link">Sobre nós</a></li>
+                        <li><a href="./index.php#contact" class="page-scroll nav-link">Contactos</a></li>
                     </ul>
                 </li>
             </nav>
@@ -69,7 +72,7 @@
 
     <!-- Main Content -->
     <main class="container">
-        <section class="newsletter-hero flex col">
+    <section class="newsletter-hero flex col">
             <section class="title">
                 <h1>Newsletter</h1>
                 <h2>Ultimas notícias</h2>
@@ -77,7 +80,7 @@
             <section class="pesquisa-filtros-container flex row j-between">
                 <form action="" method="get" class="search-form flex">
                     <input type="text" placeholder="Digite para procurar">
-                    <button type="submit"><img src="../assets/img/icon/arrow-forward.svg" alt=""></button>
+                    <button type="submit"><img src="./assets/img/icon/arrow-forward.svg" alt=""></button>
                 </form>
                 <section class="filters flex">
                     <a href="" class="ative">Tudo</a>
@@ -94,7 +97,7 @@
                 <section class="capa flex f-center">
                     <h2 class="data">21/07/2024</h2>
                     <h2 class="assunto">Finanças</h2>
-                    <img src="../assets/img/primary.png" alt="" class="newsletter-background">
+                    <img src="./assets/img/primary.png" alt="" class="newsletter-background">
                     <section class="titulo t-center">
                         <h1>Os Bancos mais lucrativos em Angola em 2024</h1>
                         <p>Bancos Angolanos Registram Lucros Recordes em 2024</p>
@@ -140,7 +143,7 @@
                 <section class="recomendation-card-area col flex f-center">
     
                         <a href="" class="newsletter-recomendation-card col flex f-center">
-                            <img src="../assets/img/micro.jpg" alt="" class="newsletter-background">
+                            <img src="./assets/img/micro.jpg" alt="" class="newsletter-background">
                             <h2 class="assunto">Startups</h2>
                             <section class="description">
                                 <h1>Conheça a startup vencedora do centro de ivest...</h1>
@@ -150,7 +153,7 @@
                         </a>
                     
                     <a class="newsletter-recomendation-card col flex f-center">
-                        <img src="../assets/img/micro.jpg" alt="" class="newsletter-background">
+                        <img src="./assets/img/micro.jpg" alt="" class="newsletter-background">
                         <h2 class="assunto">Startups</h2>
                         <section class="description">
                             <h1>Conheça a startup vencedora do centro de ivest...</h1>
@@ -158,7 +161,7 @@
                         </section>
                     </a>
                     <a class="newsletter-recomendation-card col flex f-center">
-                        <img src="../assets/img/micro.jpg" alt="" class="newsletter-background">
+                        <img src="./assets/img/micro.jpg" alt="" class="newsletter-background">
                         <h2 class="assunto">Startups</h2>
                         <section class="description">
                             <h1>Conheça a startup vencedora do centro de ivest...</h1>
@@ -166,7 +169,7 @@
                         </section>
                     </a>
                     <a class="newsletter-recomendation-card col flex f-center">
-                        <img src="../assets/img/micro.jpg" alt="" class="newsletter-background">
+                        <img src="./assets/img/micro.jpg" alt="" class="newsletter-background">
                         <h2 class="assunto">Startups</h2>
                         <section class="description">
                             <h1>Conheça a startup vencedora do centro de ivest...</h1>
@@ -178,47 +181,55 @@
         </section>
     </main>
 
-     
-    <!-- Footer -->
-    <footer class="flex col content">
-        <section class="footer-container flex row j-around a-center">
-            <section class="logo-container">
-                <img src="../assets/img/logo.svg" alt="Logo do +Kwanza" class="logo">
-            </section>
-            <section class="navegacao">
-                <h1>Navegaçao</h1>
-                <ul class="flex col">
-                    <li><a href="#hero" class="nav-link">Home</a></li>
-                    <li><a href="#podcasts" class="nav-link">Podcast</a></li>
-                    <li><a href="#about-us" class="nav-link">Sobre nós</a></li>
-                    <li><a href="#contact" class="nav-link">Contactos</a></li>
-                </ul>
-            </section>
-            <section class="suporte">
-                <h1>Suporte</h1>
-                <ul>
-                    <li>+244 999 999 999</li>
-                    <li>+244 222 222 222</li>
-                    <li>suport@maiskwanza.com</li>
-                    <li></li>
-                </ul>
-            </section>
-            <section class="newsletter-sub">
-                <h1>Newsletter</h1>
-                <p>Receba as noticias direto no seu email</p>
-                <form action="" class="newsletter-sub-form">
-                    <input type="email" placeholder="Seu email">
-                    <button type="submit" class=""><img src="../assets/img/icon/send.svg" alt="" srcset=""></button>
-                </form>
-            </section>
-        </section>
-        <section class="t-center">
-            <p>©+Kwanza All directs reserved</p>
-        </section>
-    </footer>
+   
+<!-- Footer -->
+<footer class="flex col j-evenly ">
 
-    <!-- Script -->
-    <script src="../assets/js/index.js"></script>
+    <section class="footer-container flex row j-around content ">
+        <section class="logo-container">
+            <img src="./assets/img/logo.svg" alt="Logo do +Kwanza" class="logo">
+        </section>
+        <section class="navegacao">
+            <h1>Navegação</h1>
+            <ul class="flex col">
+                <li><a href="#hero" class="nav-link">Home</a></li>
+                <li><a href="#podcasts" class="nav-link">Podcast</a></li>
+                <li><a href="#about-us" class="nav-link">Sobre nós</a></li>
+                <li><a href="#contact" class="nav-link">Contactos</a></li>
+            </ul>
+        </section>
+        <section class="suporte">
+            <h1>Suporte</h1>
+            <ul>
+                <li>+244 999 999 999</li>
+                <li>+244 222 222 222</li>
+                <li>suport@maiskwanza.com</li>
+                <li></li>
+            </ul>
+        </section>
+        <section class="newsletter-sub">
+            <h1>Newsletter</h1>
+            <p>Receba as noticias directo no seu email</p>
+            <form action="" class="newsletter-sub-form center">
+                <input type="email" placeholder="Seu email">
+                <button type="submit" class=""><img src="assets/img/icon/send.svg" alt="" srcset=""></button>
+            </form>
+        </section>
+    </section>
+    <section class="copy t-center">
+        <p>©+Kwanza 2024, Todos os direitos reservados</p>
+    </section>
+</footer>
+
+<!-- Script -->
+<script src="./assets/js/index.js"></script>
+
+<!-- Smooth Scroll -->
+<script src="./assets/js/smoothScroll/jquery.js"></script>
+<script src="./assets/js/smoothScroll/jquery.parallax.js"></script>
+<script src="./assets/js/smoothScroll/owl.carousel.min.js"></script>
+<script src="./assets/js/smoothScroll/custom.js"></script>
+
 </body>
 
 </html>
