2aec985a0518213436a79d05f9f761c51f50ebe1
H3lc1ooS <helciosilva@protonmail.com>
2024-07-24 00:33:36 +0100

    Revert "style: página de podcast e modo claro"
    This reverts commit 5fc535f4f036afacd065e5eb9eac054ce8e38838.

--- assets/js/lightMode.js
+++
@@ -1,9 +0,0 @@
-var icon = document.getElementById("colorMode");
-icon.onclick = function(){
-        document.body.classList.toggle("light-mode");
-        if(icon.tagName == 'sunny'){
-            icon.setAttribute('name', 'moon');
-        }else{
-            icon.setAttribute('name', 'sunny');
-        }
-}
