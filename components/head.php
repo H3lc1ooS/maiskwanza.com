3f3c6b81746beacf22dd77013afc6ef77970bfe1
H3lc1ooS <helciosilva@protonmail.com>
2024-07-23 23:39:52 +0100

    style:criação de componentes

---
+++ components/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content=" ">
+
+    <title> +Kwanza | Podcast</title>
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- Style -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/objects.css">
+    <link rel="stylesheet" href="./assets/css/root.css">
+    <link rel="stylesheet" href="./assets/css/midiaQ.css">
+    <link rel="stylesheet" href="./assets/css/animations.css">
+</head>
---
+++ components/scripts.php
@@ -0,0 +1,8 @@
+<!-- Script -->
+<script src="./assets/js/index.js"></script>
+
+<!-- Smooth Scroll -->
+<script src="./assets/js/smoothScroll/jquery.js"></script>
+<script src="./assets/js/smoothScroll/jquery.parallax.js"></script>
+<script src="./assets/js/smoothScroll/owl.carousel.min.js"></script>
+<script src="./assets/js/smoothScroll/custom.js"></script>
