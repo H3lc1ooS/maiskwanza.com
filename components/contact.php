c9606c5567bb7e0a66615577a0cc10c7162d9278
H3lc1ooS <helciosilva@protonmail.com>
2024-07-23 23:49:43 +0100

    style: refactoração do index

---
+++ components/contact.php
@@ -0,0 +1,25 @@
+
+        <!-- Contactos -->
+        <section id="contact" class="contacts content">
+            <section class="title">
+                <h1>Contactos</h1>
+                <h2>Saiba como nos contactar</h2>
+            </section>
+            <section class="contacts-area flex row j-between a-center ">
+                <section class="contacts-links flex col">
+                    <a href="https://www.youtube.com/@maiskwanza" class="social-link-card youtube" target="_blank">
+                        <img src="./assets/img/icon/logo-youtube.svg" alt="" class="icon">
+                        <h2 class="hide-mobile">Youtube</h2>
+                    </a>
+                    <a href="https://www.instagram.com/mais_kwanza/" class="social-link-card instagram" target="_blank">
+                        <img src="./assets/img/icon/logo-instagram.svg" alt="" class="icon">
+                        <h2 class="hide-mobile">Instagram </h2>
+                    </a>
+                    <a href="https://open.spotify.com/user/31uoo2l4ln3wtnaie3bsoetfm5hq?si=7cf32883TWikedHt4gnNoA" class="social-link-card spotify" target="_blank">
+                        <img src="./assets/img/icon/logo-spotify.svg" alt="" class="icon">
+                        <h2 class="hide-mobile">Spotify</h2>
+                    </a>
+                </section>
+                <img src="assets/img/logo.svg" alt="" srcset="" class="contact-logo">
+            </section>
+        </section>
---
+++ components/newsletter.php
@@ -0,0 +1,26 @@
+<!-- Newsletter -->
+<section class="newsletter-area content flex col a-center">
+            <section class="title">
+                <h1>Newsletter</h1>
+                <h2>Saiba o que está a acontecer no mundo dos negócios</h2>
+            </section>
+            <section class="newsletter-card-area flex row scaleup">
+                <a class="newsletter-card">
+                    <h2 class="assunto">Economia</h2>
+                    <section class="description">
+                        <h1>Os Bancos mais lucrativos em 2023</h1>
+                        <h2>Lorem ipsum dolor sit amet consectetur...</h2>
+                    </section>
+                </a>
+                <a class="newsletter-card">
+                    <h2 class="assunto">Startups</h2>
+                    <section class="description">
+                        <h1>Conheça a startup vencedora do centro de ivest...</h1>
+                        <h2>A xtal é uma startup de Lorem ipsum dolor sit amet consectetur </h2>
+                    </section>
+                </a>
+            </section>
+            <a href="./newsletter.php" class="newsletter-button scaleup">
+                <h2>Ver mais</h2>
+            </a>
+        </section>
