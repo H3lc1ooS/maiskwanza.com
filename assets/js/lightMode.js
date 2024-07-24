a39ee82378ef0981cdc98447853efbf93a3d4ff6
H3lc1ooS <helciosilva@protonmail.com>
2024-07-24 09:22:01 +0100

    fix: reverti para a versão do jodelf1

---
+++ assets/js/lightMode.js
@@ -0,0 +1,9 @@
+var icon = document.getElementById("colorMode");
+icon.onclick = function(){
+        document.body.classList.toggle("light-mode");
+        if(icon.tagName == 'sunny'){
+            icon.setAttribute('name', 'moon');
+        }else{
+            icon.setAttribute('name', 'sunny');
+        }
+}
